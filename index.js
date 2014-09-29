9a68f59ea41c2dd13bb2243767981d3eea72fd19
Ryan Breen <rbreen@vistaprint.com>
2014-09-28 22:47:18 -0400

    Add support for listing recordings that need to be tracked.

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ var tivo_broker = require('./lib/tivo_broker.js');
 logger.debug("Processing %d tivos", config.tivos.length);
 
 config.tivos.forEach(function(tivo) {
-	tivo_broker.findShows(tivo, function(err, shows) {
+	tivo_broker.findRecordings(tivo, function(err, recordings) {
 		if (err) return logger.error('Got error %s', err);
-		logger.info("Found %d shows to save", shows.length);
+
+		logger.info("There are %d recordings to save", recordings.length)
+		recordings.forEach(function(recording) {
+			if (recording.is_movie) {
+				logger.trace('Movie URL %s, title "%s"', recording.url, recording.name);
+			} else {
+				logger.trace('Show URL %s, %s episode "%s"', recording.url, recording.name, recording.episode_name);
+			}
+		});
 	});
 });
