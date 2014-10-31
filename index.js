390a79dff305b032f1069d7455ef3f852b996df8
Ryan Breen <rbreen@vistaprint.com>
2014-10-31 18:58:12 -0400

    Add downloader to serialize requests to a tivo.

--- index.js
+++ index.js
@@ -13,16 +13,19 @@ config.tivos.forEach(function(tivo) {
 	tivo_broker.findRecordings(tivo, function(err, recordings) {
 		if (err) return logger.error('Got error %s', err);
 
-		logger.info("There are %d recordings to save", recordings.length)
-		recordings.forEach(function(recording) {
-			logger.trace(util.inspect(recording));
+		// Some TiVos really don't like the download call to immediately follow all of the queries we make in findRecordings.
+		setTimeout(function() {
+			logger.info("There are %d recordings to save", recordings.length)
+			recordings.forEach(function(recording) {
+				logger.trace(util.inspect(recording));
 
-			if (!file_manager.recordingExists(recording)) {
-				logger.info("Need to store recording %s", recording.title);
-				file_manager.storeRecording(tivo, recording, function(err) {
-					if (err) return logger.error('Failed to save recording %s due to %s', recording.title, err);
-				});
-			}
-		});
+				if (!file_manager.recordingExists(recording)) {
+					logger.info("Need to store recording %s", recording.title);
+					file_manager.storeRecording(tivo, recording, function(err) {
+						if (err) return logger.error('Failed to save recording %s due to %s', recording.title, err);
+					});
+				}
+			});
+		}, 500);
 	});
 });
